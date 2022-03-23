8b271708e3366b9d5fc2b2dc8ec7fa19b85aa52d
upendra <upendrarajput9911@gmail.com>
2022-03-23 09:44:27 +0530

    done

--- block-BNaabs/simple/server.js
+++ block-BNaabs/simple/server.js
@@ -3,15 +3,24 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var app = express();
 app.use(logger('tiny'))
-app.get('/',(res,req)=>{
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/index.html')
 })
-// app.get('/new',(req,res)=>{
-//     res.sendFile(__dirname+'/new.html')
-// })
-// app.post('/new',(req,res)=>{
-    
-// })
+app.get('/new',(req,res)=>{
+    res.sendFile(__dirname+'/new.html')
+})
+app.post('/new',(req,res)=>{
+    console.log(res.body)
+   res.json(res.body)
+})
+
+app.get('/user/:username',(res,req)=>{
+    var username =req.params.username;
+    res.send(username)
+})
 app.listen(3000,()=>{
     console.log('server listening on port 3k')
 })
