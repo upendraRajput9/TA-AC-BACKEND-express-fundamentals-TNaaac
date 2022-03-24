80c7cdaf32fb399d8e36e4305530a2f5e0c4ea4e
upendra <upendrarajput9911@gmail.com>
2022-03-24 13:07:32 +0530

    done

--- block-BNaabx/simple/server.js
+++ block-BNaabx/simple/server.js
@@ -5,21 +5,19 @@ var app = express();
 //morgan logger
 app.use((err,res,req,next)=>{
     console.log(err)
-    console.log(req.methods,req.url)
+    var today = new Data();
+    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    console.log(req.methods,req.url,time)
     next()
 })
 app.use((res,req,next)=>{
-    let parase = JSON.parse(req.body)
-    console.log(parase);
+    req.body = JSON.parse(req.body)
     next()
 })
-app.use((req,res,next)=>{
-    if (req.url.split(".").pop() === "css") {
-        res.setHeader("Content-Type", "text/css");
-        res.sendFile(__dirname+"/public/stylesheet/style.css")
-      }else if(req.url.split(".").pop() === "png"){
-        res.sendFile(__dirname+"/public/stylesheet/images.css")
-      }
+app.use((path,req,res,next)=>{
+
+    req.url=req.url+path;
+    next();
 })
 app.listen(3000,()=>{
     console.log('server listening on port 3k')
