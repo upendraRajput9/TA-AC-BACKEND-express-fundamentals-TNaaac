c1f71c8382dc92f7e5c98a9669a86218fa7f1010
upendra <upendrarajput9911@gmail.com>
2022-03-24 12:29:04 +0530

    block-BNaabw-completed

--- block-BNaabw/simple/server.js
+++ block-BNaabw/simple/server.js
@@ -12,7 +12,7 @@ var app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
-app.use(express.static(__dirname+"/public"));
+app.use(express.static(__dirname+ "/public"));
 app.use(logger("dev"));
 app.use(cookieParser());
 
